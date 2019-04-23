a3df625e9f0c024ba526e2284f963ec005980fa7
mobiadd <mobile.add@mail.ru>
2019-04-23 18:23:58 +0300

    Show counter on main screen

--- app/src/main/java/ru/agentplus/romannumeralscounter/MainActivity.kt
+++ app/src/main/java/ru/agentplus/romannumeralscounter/MainActivity.kt
@@ -1,39 +1,86 @@
 package ru.agentplus.romannumeralscounter
 
+import android.content.*
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myService: CounterService
+    private var isBound = false
+    private var isPaused = true
+
+    val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            showCount(intent!!.getIntExtra("count", 0))
+        }
+    }
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(
+            className: ComponentName,
+            service: IBinder
+        ) {
+            val binder = service as CounterService.CounterBinder
+            myService = binder.service
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            isBound = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            startStopToggle()
+        }
+
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(receiver, IntentFilter(CounterService.ACTION_TICK))
+        startCounterService()
+    }
+
+    private fun startCounterService() {
+        val intent = Intent(this@MainActivity, CounterService::class.java)
+        startService(intent)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    fun startStopToggle() {
+        when {
+            isPaused -> setUnPause()
+            else -> setPause()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun setUnPause() {
+        myService.playCount()
+        isPaused = false
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun setPause() {
+        myService.pauseCount()
+        isPaused = true
     }
+
+    private fun showCount(count: Int) {
+        tv_count.text = count.toString()
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(this)
+            .unregisterReceiver(receiver)
+        unbindService(connection)
+        super.onDestroy()
+    }
+
 }
